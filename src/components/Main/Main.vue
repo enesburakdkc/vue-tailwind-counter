60168e8b71b31a597ba4afa155d13e8f6ce21138
Enes Burak Dikici <dikicienesburak@gmail.com>
2025-01-27 19:08:34 +0300

    Reset button added.

--- src/components/Main/Main.vue
+++ src/components/Main/Main.vue
@@ -10,16 +10,21 @@ const increase = () => {
 const decrease = () => {
     count.value--;
 }
+
+const reset = () => {
+    count.value = 0;
+}
 </script>
 
 <template>
-    <main class="scale-120">
-        <h1>Counter</h1>
+    <main>
+        <h1 class="font-semibold">Counter</h1>
 
-        <p class="text-4xl h-52 flex justify-center items-center">{{ count }}</p>
+        <p class="text-5xl h-48 flex justify-center items-center">{{ count }}</p>
 
         <div class="flex flex-row gap-4">
             <button @click="decrease">Decrease</button>
+            <button @click="reset">Reset</button>
             <button @click="increase">Increase</button>
         </div>
     </main>
