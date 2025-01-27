6310cdff4367f7908a1ad155f85821e6f0cb71c6
Enes Burak Dikici <dikicienesburak@gmail.com>
2025-01-27 18:35:31 +0300

    Complated counter project.

---
+++ src/components/Main/Main.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { ref } from 'vue';
+
+const count = ref(0);
+
+const increase = () => {
+    count.value++;
+}
+
+const decrease = () => {
+    count.value--;
+}
+</script>
+
+<template>
+    <main class="scale-120">
+        <h1>Counter</h1>
+
+        <p class="text-4xl h-52 flex justify-center items-center">{{ count }}</p>
+
+        <div class="flex flex-row gap-4">
+            <button @click="decrease">Decrease</button>
+            <button @click="increase">Increase</button>
+        </div>
+    </main>
+</template>
+
+<style scoped></style>
